d21221f2bd362caf4c19c5e93c6322b6d28d9a91
Yi-Hsuan Chen <yihsuan@umich.edu>
2021-09-09 09:43:02 -0400

    add dry/moist updrafts variables in the subrontine mynn_bl_driver

--- offline-edmf_mynn_v1_loop.F90
+++ offline-edmf_mynn_v1_loop.F90
@@ -3779,7 +3779,23 @@ END SUBROUTINE mym_condensation
 
     REAL,DIMENSION(KTS:KTE) :: Q_ql1,Q_qi1,Q_a1
 
-    !<--- yhc
+    !<--- yhc 2021-09-08 
+    REAL, DIMENSION(IMS:IME,KMS:KME+1,JMS:JME) :: &
+      a_moist_half , &
+      a_dry_half   , &
+      mf_moist_half, &
+      mf_dry_half  , &
+      mf_all_half  , &
+      qv_moist_half, &
+      qv_dry_half
+
+    REAL, DIMENSION(IMS:IME,KMS:KME+1,JMS:JME) :: &
+      mf_moist_full,  &
+      mf_dry_full  ,  &
+      mf_all_full  , &
+      qv_moist_full,  &
+      qv_dry_full
+
     REAL,DIMENSION(KTS:KTE+1) :: &
       a_moist_half1 , &
       a_dry_half1   , &
@@ -3795,7 +3811,7 @@ END SUBROUTINE mym_condensation
       mf_all_full1  , &
       qv_moist_full1,  &
       qv_dry_full1
-    !---> yhc
+    !---> yhc 2021-09-08
 
 ! 0 ... default thing 
 ! 1 ... new solver
@@ -4349,9 +4365,9 @@ END SUBROUTINE mym_condensation
                & s_awqv1,s_awqc1,s_awu1,s_awv1,   &
                & s_awqke1,                        &
                & qc_bm,cldfra_bm,             &
-               & a_moist_half1, mf_moist_half1, qv_moist_half1, mf_moist_full1, qv_moist_full1, &  ! yhc
-               & a_dry_half1, mf_dry_half1, qv_dry_half1, mf_dry_full1, qv_dry_full1, &            ! yhc
-               & mf_all_half1, mf_all_full1, &                                                  ! yhc
+               & a_moist_half1, mf_moist_half1, qv_moist_half1, mf_moist_full1, qv_moist_full1, &  ! yhc 2021-09-08
+               & a_dry_half1, mf_dry_half1, qv_dry_half1, mf_dry_full1, qv_dry_full1, &            ! yhc 2021-09-08
+               & mf_all_half1, mf_all_full1, &                                                     ! yhc 2021-09-08
                &ktop_shallow(i,j),ztop_shallow,   &
                & KPBL(i,j),                        &
                & Q_ql1,Q_qi1,Q_a1                 &
@@ -4604,9 +4620,22 @@ END SUBROUTINE mym_condensation
                Q_qi(i,k,j)=Q_qi1(k)
                Q_a(i,k,j)=Q_a1(k)
 
-               !<--- yhc
-
-               !---> yhc
+               !<--- yhc 2021-09-08
+               a_moist_half  (i,k,j) = a_moist_half1(k)    
+               mf_moist_half (i,k,j) = mf_moist_half1(k)    
+               mf_moist_full (i,k,j) = mf_moist_full1(k)
+               qv_moist_half (i,k,j) = qv_moist_half1(k)    
+               qv_moist_full (i,k,j) = qv_moist_full1(k)
+             
+               a_dry_half    (i,k,j) = a_dry_half1(k)    
+               mf_dry_half   (i,k,j) = mf_dry_half1(k)
+               mf_dry_full   (i,k,j) = mf_dry_full1(k)
+               qv_dry_half   (i,k,j) = qv_dry_half1(k)
+               qv_dry_full   (i,k,j) = qv_dry_full1(k)
+             
+               mf_all_half   (i,k,j) = mf_all_half1(k)
+               mf_all_full   (i,k,j) = mf_all_full1(k)
+               !---> yhc 2021-09-08
 
                ELSE
                
@@ -5357,9 +5386,9 @@ SUBROUTINE edmf_JPL(kts,kte,dt,zw,p,         &
             ! in/outputs - subgrid scale clouds
               & qc_bl1d,cldfra_bl1d,         &
             ! output info
-              & a_moist_half, mf_moist_half, qv_moist_half, mf_moist_full, qv_moist_full, &  ! yhc
-              & a_dry_half, mf_dry_half, qv_dry_half, mf_dry_full, qv_dry_full, &            ! yhc
-              & mf_all_half, mf_all_full, &                                                  ! yhc
+              & a_moist_half, mf_moist_half, qv_moist_half, mf_moist_full, qv_moist_full, &  ! yhc 2021-09-08
+              & a_dry_half, mf_dry_half, qv_dry_half, mf_dry_full, qv_dry_full, &            ! yhc 2021-09-08
+              & mf_all_half, mf_all_full, &                                                  ! yhc 2021-09-08
               &ktop,ztop,kpbl,Qql,Qqi,Qa)
 
 
